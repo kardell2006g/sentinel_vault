dad86d6f7363f4e8245a0fdb0937132b77ef9e9e
Adam K <50672995+kardell2006g@users.noreply.github.com>
2022-11-16 20:46:46 -0500

    Add files via upload

---
+++ plans/vault_jwt_bound.tf
@@ -0,0 +1,41 @@
+terraform {
+  cloud {
+    organization = "gekk0"
+
+    workspaces {
+      name = "vault_test9"
+    }
+  }
+}
+
+
+
+terraform {
+  required_providers {
+    vault = "~> 3.10.0"
+  }
+}
+
+
+provider "vault" {}
+
+resource "vault_jwt_auth_backend" "oidc" {
+  path = "oidc"
+  default_role = "test-role"
+  oidc_discovery_url    = "https://myco.auth0.com/"
+  bound_issuer          = "https://myco.auth0.com/"
+}
+
+resource "vault_jwt_auth_backend_role" "example" {
+  backend         = vault_jwt_auth_backend.oidc.path
+  role_name       = "test-role"
+  token_policies  = ["default", "dev", "prod"]
+
+  user_claim            = "https://vault/user"
+  role_type             = "oidc"
+  allowed_redirect_uris = ["http://localhost:8200/ui/vault/auth/oidc/oidc/callback"]
+  #bound_claims = {
+   # color = "red,green,blue"
+    #}
+  #bound_subject = "red"
+  }
